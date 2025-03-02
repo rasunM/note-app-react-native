daa485ee512f43b6b9739e3038a50c312d5bea48
rasunM <rasunmanohara@gmail.com>
2025-03-02 12:12:48 +0530

    added the firebase configurations

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ const Stack = createNativeStackNavigator<RootStackParamsList>();
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="TaskPage">
+      <Stack.Navigator initialRouteName="Register">
         <Stack.Screen
           name="TaskPage"
           component={TaskPage}
--- src/screens/auth/register.tsx
+++ src/screens/auth/register.tsx
@@ -8,6 +8,7 @@ import {
   Dimensions,
   NativeSyntheticEvent,
   TextInputChangeEventData,
+  Alert,
 } from 'react-native';
 import CredentialsButton from '../../components/CredentialsButton';
 import CustomButton from '../../components/CustomButton';
@@ -37,25 +38,26 @@ function RegisterPage({navigation}: RegisterPageProps) {
   };
 
   const signIn = async () => {
-    // auth()
-    //   .createUserWithEmailAndPassword(
-    //     'jane.doe@example.com',
-    //     'SuperSecretPassword!',
-    //   )
-    //   .then(() => {
-    //     console.log('User account created & signed in!');
-    //   })
-    //   .catch(error => {
-    //     if (error.code === 'auth/email-already-in-use') {
-    //       console.log('That email address is already in use!');
-    //     }
+    auth()
+      .createUserWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!',
+      )
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
 
-    //     if (error.code === 'auth/invalid-email') {
-    //       console.log('That email address is invalid!');
-    //     }
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
 
-    //     console.error(error);
-    //   });
+        console.error(error);
+      });
+    Alert.alert('Registered Successfully');
   };
 
   return (
